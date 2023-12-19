21ffc5addb6587d9f1aee46d88060ba5ff1ef460
Jeiel Lima <112661437+jeiellima@users.noreply.github.com>
2023-12-19 11:10:55 -0300

    assets + main

--- ViewController.swift
+++ ViewController.swift
@@ -11,9 +11,56 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstScreen()
     }
 
 
 }
 
+func firstScreen() {
+    lazy var startButton: UIButton = {
+        let startButton = UIButton()
+        startButton.setTitle("Começar", for: .normal)
+    }()
+}
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+    let viewController: ViewController
+
+    init(_ builder: @escaping () -> ViewController) {
+        viewController = builder()
+    }
+
+    // MARK: - UIViewControllerRepresentable
+    func makeUIViewController(context: Context) -> ViewController {
+        viewController
+    }
+
+    func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {
+        return
+    }
+}
+
+#endif
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+let deviceNames: [String] = [
+    "iPhone 11 Pro Max",
+]
+
+@available(iOS 13.0, *)
+struct ViewController_Preview: PreviewProvider {
+  static var previews: some View {
+    ForEach(deviceNames, id: \.self) { deviceName in
+      UIViewControllerPreview {
+        ViewController()
+      }.previewDevice(PreviewDevice(rawValue: deviceName))
+        .previewDisplayName(deviceName)
+    }
+  }
+}
+#endif
